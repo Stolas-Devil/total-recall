a0cf9bcfb889eb2ed917943d86c4d880f2b90f30
farkon00 <sammer2016sammer@gmail.com>
2021-09-11 17:58:05 +0300

    A fixed

--- A.py
+++ A.py
@@ -1,16 +1,17 @@
+  
 import math
 import sys
 sys.stdin = open('input.txt', 'r')
 sys.stdout = open('output.txt', 'w')
 
-A, B, C = [int(i) for i in input().split()]
+(a, b, c) = [int(s) for s in input().split()]
+(h, l) = [int(s) for s in input().split()]
 
-L, H = [int(i) for i in input().split()]
-
-sq = (A * C + B * C) * 2 + A * B
-
-fill_sq = sq * 0,85 * 1,1
-
-print(math.ceil(fill_sq / ((L * H) / 1000000)))
-
+s=2*a*c+2*b*c+a*b
+rulon=h*l/1000000
+s=s*0.85
+s=s*1.1
+s=s/rulon
+s=math.ceil(s)
+print(s)
 
