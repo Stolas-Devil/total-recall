2ae0e984fbc5ca2416ea6735813d96c5b3cced43
farkon00 <sammer2016sammer@gmail.com>
2021-09-11 17:13:07 +0300

    Add 12.py

---
+++ 12.py
@@ -0,0 +1,29 @@
+
+
+a = []
+
+def func(x):
+    final = []
+    for i in x:
+        is_min = False
+        for j in str(i):
+            if j == '-':
+                is_min = True
+            else:
+                final.append(int(j))
+    return sum(final)
+
+for i in range(int(input().split()[0])):
+    a.append([int(i) for i in input().split()])
+
+a.sort(key=func)
+
+a = a[::-1]
+
+for i in range(len(a)):
+    for j in a[i]:
+        print(j, end=' ')
+    if i != len(a) - 1:
+        print('')
+
+
