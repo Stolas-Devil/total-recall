97575caafa4a6cb39d56827560d98cab19f01259
farkon00 <sammer2016sammer@gmail.com>
2021-09-11 17:11:33 +0300

    Add 8.py

---
+++ 8.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+sys.stdout = open('output.txt', 'w')
+
+input()
+
+a = [int(i) for i in input().split()]
+
+odd = []
+even = []
+
+for i in a:
+    if i % 2 == 0:
+        odd.append(i)
+    else:
+        even.append(i)
+
+for i in odd:
+    print(i, end=" ")
+
+for i in even:
+    print(i, end=" ")
+
+
